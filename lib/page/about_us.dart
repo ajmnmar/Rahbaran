4d4b6c91194ec78ed89e581e7e105992b3bfe414
r.bazazan <ASuS@BAZAZAN>
2020-04-27 17:06:47 +0430

    aboutus

---
+++ lib/page/about_us.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:rahbaran/page/base_authorized_state.dart';
+
+class AboutUs extends StatefulWidget {
+  @override
+  AboutUsState createState() => AboutUsState();
+}
+
+class AboutUsState extends BaseAuthorizedState<AboutUs> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
